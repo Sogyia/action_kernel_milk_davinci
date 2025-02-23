9822953875c2ca5d7cabc7e1e7a3b3592b128a8a
Sogyia <tadassha@gmail.com>
2025-02-23 22:43:22 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,11 +4,8 @@
 apt remove firefox
 apt autoremove
 
-apt-get install git make bc bison
-openssl curl zip kmod cpio flex libelf-dev
-libtfm-dev wget device-tree-compiler
-python3 python2 binutils
-binutils-arm-linux-gnueabi
-
 # Environment Setup
 apt-get update && apt-get upgrade -y
+apt-get install git make bc bison
+openssl curl zip kmod cpio flex wget device-tree-compiler
+python3 python2 binutils

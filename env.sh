d5d5e4e1e235f46c922e8f1bd178a8119c3fc0e2
Sogyia <tadassha@gmail.com>
2025-02-23 22:36:23 +0200

    Update env.sh
    Add help tool

--- env.sh
+++ env.sh
@@ -4,5 +4,11 @@
 apt remove firefox
 apt autoremove
 
+apt-get install git make bc bison
+openssl curl zip kmod cpio flex libelf-dev
+libssl-dev libtfm-dev wget device-tree-compiler
+ca-certificates python3 python2 binutils
+binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi
+
 # Environment Setup
 apt-get update && apt-get upgrade -y

1780b5210a70987e70f951c1dd62dcb90bf212d2
Sogyia <tadassha@gmail.com>
2025-02-23 22:40:42 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -6,9 +6,9 @@ apt autoremove
 
 apt-get install git make bc bison
 openssl curl zip kmod cpio flex libelf-dev
-libssl-dev libtfm-dev wget device-tree-compiler
-ca-certificates python3 python2 binutils
-binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi
+libtfm-dev wget device-tree-compiler
+python3 python2 binutils
+binutils-arm-linux-gnueabi
 
 # Environment Setup
 apt-get update && apt-get upgrade -y
